aeebdb7959b0188a7cdc0660e0565f3ba581e8d3
Alex Bainter <alex.bainter@vminnovations.com>
2017-12-06 11:12:45 -0600

    Dont create a new default function every time connect is called without parameters.

--- src/connect.js
+++ src/connect.js
@@ -1,25 +1,29 @@
 import { getState } from './store';
 
-function connect(mapStateToParams, mergeParams) {
-    let mapStateToParamsFunc = mapStateToParams;
-    let mergeParamsFunc = mergeParams;
-    if (typeof mapStateToParamsFunc !== 'function') {
-        mapStateToParamsFunc = () => ({});
+const defaultMapStateToParams = () => ({});
+const defaultMergeParams = (stateParams, ownParams) =>
+  Object.assign({}, ownParams, stateParams);
+
+const connect = (mapStateToParams, mergeParams) => {
+  let mapStateToParamsFunc = mapStateToParams;
+  let mergeParamsFunc = mergeParams;
+  if (typeof mapStateToParamsFunc !== 'function') {
+    mapStateToParamsFunc = defaultMapStateToParams;
+  }
+  if (typeof mergeParamsFunc !== 'function') {
+    mergeParamsFunc = defaultMergeParams;
+  }
+  return ViewModel => {
+    if (typeof ViewModel !== 'function') {
+      throw new TypeError('ViewModel must be function.');
     }
-    if (typeof mergeParamsFunc !== 'function') {
-        mergeParamsFunc = (stateParams, ownParams) => Object.assign({}, ownParams, stateParams);
-    }
-    return function(ViewModel) {
-        if (typeof ViewModel !== 'function') {
-            throw new TypeError('ViewModel must be function.');
-        }
-        return function (ownParams) {
-            const state = getState();
-            const stateParams = mapStateToParamsFunc(state(), ownParams);
-            const mergedParams = mergeParamsFunc(stateParams, ownParams);
-            return new ViewModel(mergedParams);
-        };
+    return ownParams => {
+      const state = getState();
+      const stateParams = mapStateToParamsFunc(state(), ownParams);
+      const mergedParams = mergeParamsFunc(stateParams, ownParams);
+      return new ViewModel(mergedParams);
     };
-}
+  };
+};
 
 export default connect;
