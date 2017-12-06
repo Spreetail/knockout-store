5ac1aade689c14a958343c0ef98751b00ac832dc
Alex Bainter <alex.bainter@vminnovations.com>
2017-12-06 10:47:55 -0600

    Make mapStateToParams optional.

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,8 @@ export function setState(state: any): void;
 export function getState<T>(): KnockoutObservable<T>;
 
 export function connect(
-  mapStateToParams: (state?: any, ownParams?: object) => object,
-  mergeParams?: (stateParams: object, ownParams: object) => object
+  mapStateToParams?: (state?: any, ownParams?: any) => any,
+  mergeParams?: (stateParams: any, ownParams: any) => any
 ): (
   viewModel: KnockoutComponentTypes.ViewModelFunction
 ) => KnockoutComponentTypes.ViewModelFunction;
