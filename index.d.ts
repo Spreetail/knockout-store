44f320311b0c91647306018efc0b33b9b92c2fa7
metalex9 <alexbainter@gmail.com>
2020-05-02 21:51:43 -0500

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,34 @@
-import '@types/knockout';
+import { Observable, components } from 'knockout';
 
-export function setState(state: any): void;
+export function setState<T>(state: T): void;
 
-export function getState<T>(): KnockoutObservable<T>;
+export function getState<T>(): Observable<T>;
+
+interface ViewModelFactoryFunction {
+  (params?: components.ViewModelParams): components.ViewModel;
+}
+
+interface ViewModelInstantiator
+  extends components.ViewModelConstructor,
+    ViewModelFactoryFunction {}
+
+interface MapStateToParamsFn {
+  (
+    state?: any,
+    ownParams?: components.ViewModelParams
+  ): components.ViewModelParams;
+}
+
+interface MergeParamsFn {
+  (
+    stateParams: components.ViewModelParams,
+    ownParams: components.ViewModelParams
+  ): components.ViewModelParams;
+}
 
 export function connect(
-  mapStateToParams?: (state?: any, ownParams?: any) => any,
-  mergeParams?: (stateParams: any, ownParams: any) => any
+  mapStateToParams?: MapStateToParamsFn | null,
+  mergeParams?: MergeParamsFn | null
 ): (
-  viewModel: KnockoutComponentTypes.ViewModelFunction
-) => KnockoutComponentTypes.ViewModelFunction;
+  viewModel: components.ViewModelConstructor | ViewModelFactoryFunction
+) => ViewModelInstantiator;
